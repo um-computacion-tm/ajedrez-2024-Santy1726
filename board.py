210444862f7ee80b5f7f95c20cc13dee9df4d91d
Santiago Ortega <santy.ortega010@gmail.com>
2024-08-17 18:23:13 -0300

    Nuevo Archivo

---
+++ board.py
@@ -0,0 +1,8 @@
+class Board:
+    def __init__(self):
+        self.positions = []
+        for _ in range(8):
+            col = []
+            for _ in range(8):
+                col.append(None)
+            self.positions.append(col)
