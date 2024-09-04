e03c8c406c320c8ec9aa0f93592510fd40a7c3c6
Santiago Ortega <santy.ortega010@gmail.com>
2024-09-04 19:09:19 -0300

    se le ha agregado la funciones de detectar se hay un espacio ocupado por otra pieza del mismo color

--- Piezas/horse.py
+++ Piezas/horse.py
@@ -10,6 +10,24 @@ class Horse:
             return "♞"      # símbolo para el caballo negro
 
 
+    def esta_dentro_del_tablero(self, x, y):
+        return 0 <= x < 8 and 0 <= y < 8
+
+    def esta_ocupada_por_mismo_color(self, x, y):
+        pieza = self.__obtener_pieza_en_posicion__(x, y)
+        return pieza is not None and pieza.__color__ == self.__color__
+
+    def esta_ocupada(self, x, y):
+        pieza = self.__obtener_pieza_en_posicion__(x, y)
+        return pieza is not None
+
+    def obtener_pieza_en_posicion(self, x, y):
+        pass
+
+    def __calcular_movimientos_validos(self):
+        movimientos_validos = []
+        x, y = self.__posicion_actual__
+
     def mov_del_horse(self, posicion_actual):
         fila_actual, columna_actual = posicion_actual
         movimientos = [
