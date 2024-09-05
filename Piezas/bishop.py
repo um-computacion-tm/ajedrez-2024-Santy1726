bde431cbe6ac9da134c6eade1f9614bb3a413381
Santiago Ortega <santy.ortega010@gmail.com>
2024-09-05 20:15:12 -0300

    creacion de los movimientos del bishop

--- Piezas/bishop.py
+++ Piezas/bishop.py
@@ -1,5 +1,37 @@
 class Bishop:
-    def__init__(self, color)
-    self.__color___ = color 
+    def __init__(self, fila, columna, color):
+        self.fila = fila
+        self.columna = columna
+        self.color = color
+
+    def mov(self, posicion, tablero):
+        fila, columna = posicion
+        movimientos_validos = []
+
+        direcciones = [(-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+        # Explorar cada dirección
+        for direccion in direcciones:
+            dx, dy = direccion
+            x, y = fila + dx, columna + dy
+
+          
+            while 0 <= x < 8 and 0 <= y < 8:
+                # Si la casilla está ocupada
+                if tablero.esta_ocupada(x, y):
+                    # Si es una pieza del color contrario, agregar como movimiento válido
+                    if not tablero.esta_ocupada_por_mismo_color(x, y, self.color):
+                        movimientos_validos.append((x, y))
+                    break
+                else:
+                    movimientos_validos.append((x, y))
+                
+                # Avanzar en la misma dirección
+                x += dx
+                y += dy
+
+        return movimientos_validos
+
+
 
     
